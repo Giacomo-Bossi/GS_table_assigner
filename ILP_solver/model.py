e3d290b04d76f961513cd45b3b07acf1644c95c2
Giacomo-Bossi <giacomo3.bossi@mail.polimi.it>
2025-05-02 18:15:27 +0200

    minor refactoring of model

--- ILP_solver/model.py
+++ ILP_solver/model.py
@@ -2,82 +2,83 @@ import json
 import mip
 from utils import input_id_checker
 
-INPUT_FILE = "input.json"
-OUTPUT_FILE = "output.json"
-
-def input_reader():
-    with open(INPUT_FILE, 'r') as f:
-        data = json.load(f)
-        tables = data['tables'] 
-        guests = data['groups']
-        return tables, guests
-    
 def calculate_table_penalty(num_tables : int)->float:
     """
     assign the table penalty in a way that does not affect the number of people that can fit
     """
     return 1/(num_tables+1)
 
-
-tables, guests = input_reader()
-
-tables_ids = []
-tables_capacities = []
-for tab in tables:
-    tables_ids.append(tab["id"])
-    tables_capacities.append(tab["capacity"])
-
-groups_ids = []
-group_sizes = []
-
-for grp in guests:
-    groups_ids.append(grp["id"])
-    group_sizes.append(grp["size"])
-
-#for future, add preferences
-num_tables = len(tables)
-num_reserv = len(guests)
-
-table_penalty = calculate_table_penalty(num_tables)
-
-ok = input_id_checker(tables_ids,groups_ids)
-if not ok:
-    raise ValueError("Invalid table or group IDs in the input data.")
-
-model = mip.Model(sense=mip.MAXIMIZE)
-
-#Variables
-
-assignement = [[model.add_var(var_type= mip.BINARY) for _ in tables_ids]for _ in groups_ids]#2D array with all variables
-used_tables = [model.add_var(var_type=mip.BINARY)for _ in tables_ids]
-
-#table capacity contstraint and atble usage
-for id in tables_ids:
-    model.add_constr(mip.xsum(assignement[grp_id][id]*group_sizes[grp_id] for grp_id in groups_ids) <= tables_capacities[id])
-    model.add_constr(mip.xsum(assignement[grp_id][id] for grp_id in groups_ids)<=num_tables * used_tables[id])
+def solve_instance(json_string :str)->str:
+    """
+    This function takes a json string describing tables and reservations and return a json string with the found solution to the problem
+    
+    """
+    tables = json_string['tables'] 
+    guests = json_string['groups']
 
 
-#single assignement
-for g_id in groups_ids:
-   model.add_constr(mip.xsum(assignement[g_id][tab_id] for tab_id in tables_ids) <= 1)
+    tables_ids = []
+    tables_capacities = []
+    for tab in tables:
+        tables_ids.append(tab["id"])
+        tables_capacities.append(tab["capacity"])
 
-model.objective = mip.xsum(assignement[grp_id][tab_id]*group_sizes[grp_id] for tab_id in tables_ids for grp_id in groups_ids) - table_penalty * mip.xsum(used_tables)
+    groups_ids = []
+    group_sizes = []
 
-model.optimize()
+    for grp in guests:
+        groups_ids.append(grp["id"])
+        group_sizes.append(grp["size"])
 
-output_data = []
-if model.num_solutions:
-    for grp_id in range(len(groups_ids)):
-        for tab_id in range(len(tables_ids)):
-            if assignement[grp_id][tab_id].x >= 0.99:
-                print(f"Group {groups_ids[grp_id]} is assigned to table {tables_ids[tab_id]}")
-                output_data.append({"group_id": grp_id, "table_id": tab_id, "group_size":group_sizes[grp_id]})
-else:
-    print("No solution found")
+    #for future, add preferences
+    num_tables = len(tables)
+    num_reserv = len(guests)
+
+    table_penalty = calculate_table_penalty(num_tables)
+
+    #should be already checked before
+    ok = input_id_checker(tables_ids,groups_ids)
+    if not ok:
+        return json.dumps({{"error": "Invalid format on the ids"}}).encode("utf-8")
+
+    model = mip.Model(sense=mip.MAXIMIZE)
+
+    assignement = [[model.add_var(var_type= mip.BINARY) for _ in tables_ids]for _ in groups_ids]#2D array with all variables
+    used_tables = [model.add_var(var_type=mip.BINARY)for _ in tables_ids]
+
+    #assign at most as the table capacity and linking constraint for used_tables
+    for id in tables_ids:
+        model.add_constr(mip.xsum(assignement[grp_id][id]*group_sizes[grp_id] for grp_id in groups_ids) <= tables_capacities[id])
+        model.add_constr(mip.xsum(assignement[grp_id][id] for grp_id in groups_ids)<=num_tables * used_tables[id])
 
 
-for line in output_data:
-    with open(OUTPUT_FILE,'w') as f:
-        json.dump(output_data, f, indent=2)
+    #assign a group to at most one table
+    for g_id in groups_ids:
+        model.add_constr(mip.xsum(assignement[g_id][tab_id] for tab_id in tables_ids) <= 1)
+
+    model.objective = mip.xsum(assignement[grp_id][tab_id]*group_sizes[grp_id] for tab_id in tables_ids for grp_id in groups_ids) - table_penalty * mip.xsum(used_tables)
+
+    model.optimize()
+
+    #organize optimal solution in json
+    output_data = {"pairings": [],"used_tables" : 0}
+    pairs = []
+
+    if model.num_solutions:
+        for grp_id in range(len(groups_ids)):
+            for tab_id in range(len(tables_ids)):
+                if assignement[grp_id][tab_id].x >= 0.99:
+                    pairs.append({"group_id": grp_id, "table_id": tab_id, "group_size":group_sizes[grp_id]})
+
+        total_tables_used = sum(used_tables[i].x for i in range(len(used_tables)))
+
+        output_data['pairings'] = pairs
+        output_data['used_tables']=int(total_tables_used)
+
+    else:
+        print("No solution found")
+
+    return output_data
+
 
 
