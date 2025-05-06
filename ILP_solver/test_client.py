a085831b61fee0b5618fef2dea39abb0b831fda5
Giacomo-Bossi <giacomo3.bossi@mail.polimi.it>
2025-05-07 00:59:24 +0200

    updated ouptut_shema and output validation

--- ILP_solver/test_client.py
+++ ILP_solver/test_client.py
@@ -10,13 +10,15 @@ data = JSON_generate_input([24,5,4],[4,23])
 
 with open("schemas/input_schema.json", "r") as schema_file:  
         schema = json.load(schema_file)
-
+with open("schemas/output_schema.json",'r') as schema_file:
+        schema2 = json.load(schema_file)
 try:
     validate(schema, data)
     print("input was validated")
     response = requests.post(url,json=data)
     print("response status :",response.status_code)
     print("content :", response.text)
+    validate(schema2, json.loads(response.text))
 except requests.exceptions.RequestException as e:
     print("Error : ",e)
 except ValidationError as e:
