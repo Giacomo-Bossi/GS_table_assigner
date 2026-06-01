7fe26e40fb3c8aa2ab4a7dee201f410bd5a9d503
Eyrtxnuo <mattiabossi2004@gmail.com>
2026-06-01 21:30:31 +0200

    aggiunto supporto agli warning nella lista lavori

--- v2/webpage/scripts/list.js
+++ v2/webpage/scripts/list.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
             const jobsList = $('#tableBody');
             
             for (const [idx, jobId] of data.jobs.entries()){  
-                jobsList.append(`<tr id="${jobId}">
+                jobsList.append(`<tr id="${jobId}" class="jobRow">
                     <td>${data.jobs.length - idx}</td>
                     <td ></td>
                     <td><i class="ri-loader-2-line jobIcon execJob"></i></td>
-                    <td><button><i class="ri-file-paper-2-fill" onclick="document.location.href='solver/download/${jobId}/placeholders'"></i></button><button><i class="ri-treasure-map-line" onclick="document.location.href='solver/download/${jobId}/map'"></i></button></td>
+                    <td><button onclick="document.location.href='solver/download/${jobId}/placeholders'"><i class="ri-file-paper-2-fill" ></i></button><button onclick="document.location.href='solver/download/${jobId}/map'"><i class="ri-treasure-map-line" ></i></button></td>
                 </tr>`);
                 
                 const updateJobStatus = () => {
@@ -17,15 +17,23 @@ $(document).ready(function() {
                         .then(response => response.json())
                         .then(statusData => {
                             
-                                console.log(statusData);
+                            const assignedAll = statusData.result && statusData.result["total assignable"] ==  statusData.result["total guests"];
+                            const hasWarnings = statusData.result && statusData.result["warnings"] && statusData.result["warnings"].length > 0;
+                            console.log(statusData);
+
                             // Update the status column based on the fetched status
                             const statusCell = $(`#${jobId} td:nth-child(3)`);
                             const numsCell = $(`#${jobId} td:nth-child(2)`);
+                            const btnsCell = $(`#${jobId} td:nth-child(4)`);
                             if (statusData.status === "COMPLETED") {
-                                if(statusData.result && statusData.result["total assignable"] ==  statusData.result["total guests"] ){
+                                if(assignedAll && !hasWarnings){
                                     statusCell.html('<i class="ri-file-check-line jobIcon doneJob"></i>');
                                     numsCell.html(`${statusData.result["total assignable"]} / ${statusData.result["total guests"]}`);
                                     numsCell.addClass("ok");
+                                } else if(assignedAll && hasWarnings){
+                                    statusCell.html('<i class="ri-file-warning-line jobIcon warnJob"></i>');
+                                    numsCell.html(`${statusData.result["total assignable"]} / ${statusData.result["total guests"]}`);
+                                    numsCell.addClass("warn");
                                 } else {
                                     statusCell.html('<i class="ri-file-warning-line jobIcon failJob"></i>');
                                     numsCell.html(`${statusData.result["total assignable"]} / ${statusData.result["total guests"]}`);
@@ -38,7 +46,13 @@ $(document).ready(function() {
                             } else if (statusData.status === "PROGRESS") {
                                 numsCell.html(`${statusData.meta.current} / ${statusData.meta.total}`);
                             } else {
-                                statusCell.html(`<i class="ri-error-warning-line jobIcon err"></i> ${statusData.status}`);
+                                numsCell.html(`<p>${statusData.status}</p>`);
+                                statusCell.html(`<i class="ri-error-warning-line jobIcon err"></i>`);
+                            }
+
+
+                            if(hasWarnings){
+                                btnsCell.append('<button onclick="document.location.href=\'job.html?job_id=' + jobId + '\'"><i class="ri-feedback-line" ></i></button>');
                             }
                         })
                         .catch(error => console.error(`Error fetching status for job ${jobId}:`, error));
